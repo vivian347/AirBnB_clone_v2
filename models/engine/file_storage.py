5d0e5028a0e353f1fb12411d06806474b8d02b41
vivian347 <vkinyua001@gmail.com>
2023-01-19 10:59:47 +0300

    idk

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,79 +1,70 @@
 #!/usr/bin/python3
 """
-    Define class FileStorage
+Contains the FileStorage class
 """
+
 import json
-import models
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class FileStorage:
-    """
-        Serializes instances to JSON file and deserializes to JSON file.
-    """
+    """serializes instances to a JSON file & deserializes back to instances"""
+
+    # string - path to the JSON file
     __file_path = "file.json"
+    # dictionary - empty but will store all objects by <class name>.id
     __objects = {}
 
     def all(self, cls=None):
-        """
-            Return the dictionary
-        """
-        if cls is None:
-            return self.__objects
-        else:
-            my_dict = {}
-            for k, v in self.__objects.items():
-                name = k.split('.')
-                if name[0] in str(cls):
-                    my_dict[k] = v
-            return my_dict
+        """returns the dictionary __objects"""
+        if cls is not None:
+            new_dict = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__ or cls == value.__class__.__name__:
+                    new_dict[key] = value
+            return new_dict
+        return self.__objects
 
     def new(self, obj):
-        """
-            Set in __objects the obj with key <obj class name>.id
-            Aguments:
-                obj : An instance object.
-        """
-        key = str(obj.__class__.__name__) + "." + str(obj.id)
-        value_dict = obj
-        FileStorage.__objects[key] = value_dict
+        """sets in __objects the obj with key <obj class name>.id"""
+        if obj is not None:
+            key = obj.__class__.__name__ + "." + obj.id
+            self.__objects[key] = obj
 
     def save(self):
-        """
-            Serializes __objects attribute to JSON file.
-        """
-        objects_dict = {}
-        for key, val in FileStorage.__objects.items():
-            objects_dict[key] = val.to_dict()
-
-        with open(FileStorage.__file_path, mode='w', encoding="UTF8") as fd:
-            json.dump(objects_dict, fd)
+        """serializes __objects to the JSON file (path: __file_path)"""
+        json_objects = {}
+        for key in self.__objects:
+            json_objects[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(json_objects, f)
 
     def reload(self):
-        """
-            Deserializes the JSON file to __objects.
-        """
+        """deserializes the JSON file to __objects"""
         try:
-            with open(FileStorage.__file_path, encoding="UTF8") as fd:
-                FileStorage.__objects = json.load(fd)
-            for key, val in FileStorage.__objects.items():
-                class_name = val["__class__"]
-                class_name = models.classes[class_name]
-                FileStorage.__objects[key] = class_name(**val)
-        except FileNotFoundError:
+            with open(self.__file_path, 'r') as f:
+                jo = json.load(f)
+            for key in jo:
+                self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
+        except:
             pass
 
     def delete(self, obj=None):
-        """
-        Deletes an object from __objects if it is inside of __objects
-        """
-        copy_storage = dict(FileStorage.__objects)
-        desired_key = obj
-        for key, val in copy_storage.items():
-            if val == desired_key:
-                del(obj)
-                del FileStorage.__objects[key]
-                self.save()
+        """delete obj from __objects if it’s inside"""
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
 
     def close(self):
-        """ deserializes json file to objects """
+        """call reload() method for deserializing the JSON file to objects"""
         self.reload()
--- models/state.py
+++ models/state.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" holds class State"""
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-import os
-import models
-from models.city import City
 
 
-class State(BaseModel):
-    """ State class """
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tabename__ = "states"
+class State(BaseModel, Base):
+    """Representation of state """
+    if models.storage_t == "db":
+        __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state",
-                                cascade="delete")
+        cities = relationship("City", backref="state")
     else:
         name = ""
 
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
         @property
         def cities(self):
-            city_dict = model.storage.all(City)
-            state_query = self.id
+            """getter for list of city instances related to the state"""
             city_list = []
-            for k, v in city_dict.items():
-                if v.state_id == self.id:
-                    city_list.apend(v)
-            return city_list
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return
